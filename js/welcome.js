43d442d3ab89ced25a80e38079a13683a499f763
Robert <72618337+Robert-Stackflow@users.noreply.github.com>
2023-03-07 09:18:45 +0800

    Site updated: 2023-03-07 09:18:34

--- js/welcome.js
+++ js/welcome.js
@@ -6,14 +6,7 @@ if (localStorage.getItem("popWelcomeWindow") != "0") {
 }
 if (sessionStorage.getItem("popCookieWindow") != "0") {
     setTimeout(function () {
-        Snackbar.show({
-            text: '本站使用Cookie和本地/会话存储保证浏览体验和网站统计',
-            pos: 'bottom-right',
-            actionText: "查看博客声明",
-            onActionClick: function (element) {
-                window.open("/license")
-            },
-        })
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.cookie_notice)
     }, 3000)
 }
 sessionStorage.setItem("popCookieWindow", "0");
