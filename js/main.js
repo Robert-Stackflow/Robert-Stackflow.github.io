bba20483bf15bde26d80714d5e8f162d7dd837a7
Robert <72618337+Robert-Stackflow@users.noreply.github.com>
2023-03-10 11:31:56 +0800

    Site updated: 2023-03-10 11:31:47

--- js/main.js
+++ js/main.js
@@ -67,11 +67,11 @@ document.addEventListener("DOMContentLoaded", function () {
   function isHome() {
     if (
       window.document.location.href != "http://localhost:4000/" &&
-      window.document.location.href != "http://clouchewie.com/" &&
+      window.document.location.href != "http://cloudchewie.com/" &&
       window.document.location.href != "http://www.cloudchewie.com" &&
       window.document.location.href != "http://github.cloudchewie.com" &&
       window.document.location.href != "https://localhost:4000/" &&
-      window.document.location.href != "https://clouchewie.com/" &&
+      window.document.location.href != "https://cloudchewie.com/" &&
       window.document.location.href != "https://www.cloudchewie.com" &&
       window.document.location.href != "https://github.cloudchewie.com"
     )
